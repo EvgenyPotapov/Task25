f27455e5b6d8e915c0fc08ce69933b57d4b32d88
Evgenii Potapov <evgeny-potapov@mail.ru>
2023-02-13 18:31:21 +0300

    рещение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную
+// степень B. Реализовать функцию возведения в степень
+// самостоятельно!
+
+Console.Clear();
+
+double MathPow(int num, int st)
+{
+    double n = Math.Pow(num, st);
+    return n; 
+}
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+double math = MathPow(A, B);
+Console.WriteLine($"{A}, {B} -> {math}");
